09f2370092f38cfb46f2824c9959dfe04a36f50a
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-02-07 16:11:14 +0000

    Drop dependency on the jellyfish-mail package
    The abstraction inside `jellyfish-mail` can be held entirely inside the
    `action-send-email` action, allowing us to archive the whole
    `jellyfish-mail` repo.
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/actions/action-send-email.ts
+++ lib/actions/action-send-email.ts
@@ -1,11 +1,77 @@
 import { defaultEnvironment } from '@balena/jellyfish-environment';
-import mail, { SendEmailOptions } from '@balena/jellyfish-mail';
 import type { Contract } from '@balena/jellyfish-types/build/core';
 import type { ActionDefinition } from '@balena/jellyfish-worker';
+import { MailOptions } from '@balena/jellyfish-environment';
+import axios from 'axios';
+import FormData from 'form-data';
 
 const MAIL_OPTIONS = defaultEnvironment.mail.options || {
 	domain: '',
 };
+
+interface SendEmailOptions {
+	toAddress: string;
+	fromAddress: string;
+	subject: string;
+	html: string;
+}
+
+class Mailgun {
+	public options: MailOptions;
+	public domain: string;
+	public requestDomain: string;
+	public auth: {
+		user: string;
+		pass: string;
+	};
+
+	constructor(options: MailOptions) {
+		this.options = options;
+		this.domain = options.domain;
+		this.requestDomain = `${this.options.baseUrl}/${this.domain}`;
+		this.auth = {
+			user: 'api',
+			pass: this.options.token,
+		};
+	}
+
+	/**
+	 * @summary Send email using Mailgun api
+	 * @function
+	 *
+	 * @param options - send email options
+	 * @returns API request result
+	 *
+	 * @example
+	 * ```typescript
+	 * const result = await sendEmail({
+	 *   toAddress: 'user@domain.com',
+	 *   fromAddress: 'us@domain.com',
+	 *   subject: 'Hello',
+	 *   html: '<b>Hello</b>',
+	 * });
+	 * ```
+	 */
+	public async sendEmail(options: SendEmailOptions): Promise<any> {
+		const from = options.fromAddress
+			? options.fromAddress
+			: `Jel.ly.fish <no-reply@${this.domain}>`;
+
+		const form = new FormData();
+		form.append('from', from);
+		form.append('to', options.toAddress);
+		form.append('subject', options.subject);
+		form.append('html', options.html);
+
+		return await axios.post(`${this.requestDomain}/messages`, form, {
+			auth: {
+				username: this.auth.user,
+				password: this.auth.pass,
+			},
+			headers: form.getHeaders(),
+		});
+	}
+}
 
 /**
  * @summary Build and return send email request options.
@@ -42,8 +108,12 @@ const handler: ActionDefinition['handler'] = async (
 ) => {
 	const { fromAddress, toAddress, subject, html } = request.arguments;
 
-	if (mail) {
-		const response = await mail.sendEmail({
+	if (
+		defaultEnvironment.mail.type === 'mailgun' &&
+		defaultEnvironment.mail.options
+	) {
+		const client = new Mailgun(defaultEnvironment.mail.options);
+		const response = await client.sendEmail({
 			toAddress,
 			fromAddress,
 			subject,
