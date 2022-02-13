a6f34672c50179073d5c1fb00fb8cf3b66ba6afb
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-02-13 18:33:11 +0000

    milestone: Add a progress bar
    This change adds a progress bar on milestones, that fills up as attached
    issues are closed.
    
    Fixes https://github.com/product-os/jellyfish-plugin-default/issues/957
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/contracts/milestone.ts
+++ lib/contracts/milestone.ts
@@ -28,15 +28,55 @@ export const milestone: ContractDefinition = cardMixins.mixin(
 					type: 'string',
 					fullTextSearch: true,
 				},
+				data: {
+					type: 'object',
+					properties: {
+						milestonesPercentComplete: {
+							title: 'Milestones progress',
+							default: 0,
+							type: 'number',
+							readOnly: true,
+							// eslint-disable-next-line max-len
+							$$formula: `
+								contract.links["is attached to"] && contract.links["is attached to"].length ? (
+									FILTER(contract.links["is attached to"], { type: "issue@1.0.0", data: { status: "closed" } }).length / 
+									FILTER(contract.links["is attached to"], { type: "issue@1.0.0" }).length
+								) * 100 : 0
+							`,
+						},
+					},
+				},
 			},
 			required: ['name'],
 		},
 		uiSchema: {
+			fields: {
+				data: {
+					'ui:order': ['status', 'percentComplete'],
+					percentComplete: {
+						'ui:widget': 'ProgressBar',
+						'ui:options': {
+							success: true,
+							alignSelf: 'stretch',
+							alignItems: 'stretch',
+						},
+					},
+				},
+			},
 			snippet: {
 				data: {
+					'ui:order': ['status', 'percentComplete'],
 					status: {
 						'ui:title': null,
 						'ui:widget': 'Badge',
+					},
+					percentComplete: {
+						'ui:widget': 'ProgressBar',
+						'ui:options': {
+							success: true,
+							alignSelf: 'stretch',
+							alignItems: 'stretch',
+						},
 					},
 				},
 			},
