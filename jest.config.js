e3503952804038a55e7ccf6a9bbc04cb721af8a7
Josh Bowling <josh@balena.io>
2022-02-24 12:39:39 +0900

    Transform serialize-error
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@ const base = require('@balena/jellyfish-config/config/jest.config')
 
 module.exports = {
 	...base,
-	testTimeout: 60000
+	testTimeout: 60000,
+	transformIgnorePatterns: [
+		"/node_modules/(?!serialize-error)",
+	],
+	transform: {
+		"/node_modules/serialize-error/(.*)": 'jest-esm-transformer'
+	}
 };
