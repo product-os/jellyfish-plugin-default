db066cf4b8faaceb0f9088fa47842100437dede1
Josh Bowling <josh@balena.io>
2022-03-01 17:45:39 +0900

    Remove jest esm transforms
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,5 @@ const base = require('@balena/jellyfish-config/config/jest.config')
 
 module.exports = {
 	...base,
-	testTimeout: 60000,
-	transformIgnorePatterns: [
-		"/node_modules/(?!serialize-error)",
-	],
-	transform: {
-		"/node_modules/serialize-error/(.*)": 'jest-esm-transformer'
-	}
+	testTimeout: 60000
 };
